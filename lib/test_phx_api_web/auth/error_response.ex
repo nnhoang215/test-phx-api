9ba5e52b55f5cbad8d742a457dbdf578313735d3
nnhoang215 <nnhoang215@gmail.com>
2023-05-18 18:47:40 +1000

    refresh_token

--- lib/test_phx_api_web/auth/error_response.ex
+++ lib/test_phx_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule TestPhxApiWeb.Auth.ErrorResponse.Forbidden do
   defexception [message: "You do not have access to this resource.", plug_status: 403]
 end
+
+defmodule TestPhxApiWeb.Auth.ErrorResponse.NotFound do
+  defexception [message: "Not Found", plug_status: 404]
+end
